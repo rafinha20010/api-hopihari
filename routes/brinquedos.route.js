9122444f12baf66b5e386b767b969472d1717281
rafinha20010 <rafaelsanchesperacine@gmail.com>
2025-05-27 10:10:58 -0300

    asas

--- routes/brinquedos.route.js
+++ routes/brinquedos.route.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const login = require('../middleware/usuarios.middleware');
+const brinquedosController = require("../controllers/brinquedos.controller");
 
+router.post("/",
+    login.required,
+    login.userRequired,
+    brinquedosController.cadastroBrinquedo
+);
 
-
+router.get("/area/:areaName", login.required,
+    brinquedosController.getBrinquedosByArea
+);
 
 module.exports = router; 
